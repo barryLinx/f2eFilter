2f078f017974d3912884c03494d2866cce9042ad
Bluce <a1019521a@gmail.com>
2021-07-04 14:00:38 +0800

    mobile tablet & Pad 尺寸調整

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,15 @@
 import '../styles/scss/index.scss';
 import '../assets/image/logo.png';
+import '../assets/image/photo1.jpg';
+import '../assets/image/photo2.jpg';
+import '../assets/image/photo3.jpg'
 import { library } from '@fortawesome/fontawesome';
 import { faTimes,faMapMarker, faSearch,faPlus} from '@fortawesome/fontawesome-free-solid';
 import { faCalendarAlt } from '@fortawesome/fontawesome-free-regular';
 
 library.add(faTimes,faMapMarker,faSearch,faCalendarAlt,faPlus)
 
+import 'bootstrap/js/dist/collapse';
 //<i class="fas fa-times"></i>
 //<i class="fas fa-map-marker-alt"></i>
 //<i class="fas fa-search"></i>
